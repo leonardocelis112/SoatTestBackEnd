f1f10aeebba71887eeb85513a4c44a41a80d7475
Leonardo Celis <leonardocelis112@gmail.com>
2017-02-08 16:30:30 -0500

    Change locales to deploy on heroku

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module SoatTestBackEnd
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     I18n.default_locale = :es
     config.api_only = true
   end
