cd97dcfe596450bebbaddca2b8cf6b52319445da
Leonardo Celis <leonardocelis112@gmail.com>
2017-02-09 16:24:45 -0500

    Minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 # Class for manage user model
 class User < ApplicationRecord
   rolify
+  before_create :set_default_role
   has_secure_password
   has_many :soat_insurances
 
   validates :email, presence: true
+
+  private
+
+  def set_default_role
+    add_role(:client)
+  end
 end
